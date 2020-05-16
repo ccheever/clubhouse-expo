4805d2cc3e557ec79f06cd54873633b84d64ce8f
Charlie Cheever <ccheever@gmail.com>
2020-05-16 04:04:13 -0700

    First screen look sort of right

--- clubhouse/App.tsx
+++ clubhouse/App.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image, StyleSheet, TouchableOpacity, Text, View } from "react-native";
+
+import { useFonts, Nunito_600SemiBold } from "@expo-google-fonts/nunito";
+import { useDimensions } from "@react-native-community/hooks";
+import { AppLoading } from "expo";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return <SigninScreen />;
+}
+
+function SigninScreen() {
+  let { height, width } = useDimensions().window;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#F1EFE5",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        paddingBottom: 60,
+      }}
+    >
+      <Image
+        source={require("./assets/onboarding_start.png")}
+        style={{
+          height: 300,
+          width: 300,
+          marginBottom: 150,
+        }}
+      />
+
+      <TouchableOpacity onPress={() => {}}>
+        <View
+          style={{
+            height: 48,
+            width: 240,
+            borderRadius: 24,
+            backgroundColor: "#5C75A8",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "Nunito_600SemiBold",
+              fontSize: 20,
+              color: "white",
+            }}
+          >
+            Sign in
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +66,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
